a57f6749e10b6a73ea0fa0aef4675688bb311eb4
Alvin <you@example.com>
2021-05-22 13:21:16 +0700

    rev2

--- resources/views/artikel0252.blade.php
+++ resources/views/artikel0252.blade.php
@@ -30,44 +30,62 @@
     </header>
     <!--content-->
     <div class="container-fluid p-3 mb-2 bg-light text-dark">
+      <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Laravel</h5>
+          <p class="card-text">Laravel adalah framework ...</p>
+          <a href="#" class="btn btn-primary">Open </a>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">CakePHP</h5>
+            <p class="card-text">CakePHP adalah framework ...</p>
+            <a href="#" class="btn btn-primary">Open </a>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Symphony</h5>
+            <p class="card-text">Symphony adalah framework ...</p>
+            <a href="#" class="btn btn-primary">Open </a>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Zend</h5>
+            <p class="card-text">Zend adalah framework ...</p>
+            <a href="#" class="btn btn-primary">Open </a>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Codeigniter</h5>
+            <p class="card-text">Codeigniter adalah framework ...</p>
+            <a href="#" class="btn btn-primary">Open </a>
+          </div>
+        </div>
+      </div>
       <h1>Pengenalan Framework</h1>
       <h2>Apa Itu Framework?</h2>
       <p>Ketika mengacu pada software komputer, pengertian framework adalah fondasi dengan tingkat kerumitan yang ditentukan dan dapat diperluas oleh pemrogram dengan menggunakan kode mereka sendiri termasuk kompiler, juru bahasa, atau API. Secara umum, framework menyediakan lingkungan yang memfasilitasi jenis pemrograman tertentu untuk proyek pengembangan software. Sebuah framework dalam pemrograman seringkali merupakan struktur berlapis yang menunjukkan program apa yang dapat atau harus dibangun dan bagaimana framework akan saling terkait. Beberapa framework sistem komputer juga termasuk program aktual, menentukan interface pemrograman, atau menawarkan alat pemrograman untuk menggunakan kerangka kerja. Suatu framework mungkin digunakan untuk sekumpulan fungsi dalam suatu sistem dan bagaimana semuanya saling terkait. Apa itu Framework? Framework bisa berarti lapisan-lapisan sistem operasi dan lapisan-lapisan suatu subsistem aplikasi, bagaimana komunikasi harus distandarisasi pada beberapa tingkat jaringan dan seterusnya. Framework Umumnya lebih komprehensif daripada protokol dan lebih bersifat perspektif daripada struktur.</p>
       <h2>Framework PHP Terbaik</h2>
-      <p>PHP yang dikenal sebagai bahasa server-side scripting paling populer di dunia telah banyak berkembang sejak potongan kode inline pertama muncul dalam file HTML statis.
-
-        Pengembang perlu membangun situs web dan aplikasi yang kompleks dan dengan kerumitan tertentu mungkin butuh terlalu banyak waktu untuk selalu memulai dari awal, sehingga muncul kebutuhan akan cara pengembangan yang lebih terstruktur.
-        
-        Framework PHP memberi solusi yang memudahkan untuk membangun situs web dan aplikasi yang kompleks.
-        
-        Framework PHP memberikan kode yang terorganisir dengan baik, dapat digunakan kembali, dan dijaga. Menghindarkan Anda dari keamanan situs yang rendah. Mengikuti  pola MVC (Model-View-Controller) yang memastikan pemisahan presentasi dan logika. Mempromosikan praktik pengembangan web modern seperti alat pemrograman berorientasi objek.</p>
+      <p>PHP yang dikenal sebagai bahasa server-side scripting paling populer di dunia telah banyak berkembang sejak potongan kode inline pertama muncul dalam file HTML statis.Pengembang perlu membangun situs web dan aplikasi yang kompleks dan dengan kerumitan tertentu mungkin butuh terlalu banyak waktu untuk selalu memulai dari awal, sehingga muncul kebutuhan akan cara pengembangan yang lebih terstruktur. Framework PHP memberi solusi yang memudahkan untuk membangun situs web dan aplikasi yang kompleks. Framework PHP memberikan kode yang terorganisir dengan baik, dapat digunakan kembali, dan dijaga. Menghindarkan Anda dari keamanan situs yang rendah. Mengikuti  pola MVC (Model-View-Controller) yang memastikan pemisahan presentasi dan logika. Mempromosikan praktik pengembangan web modern seperti alat pemrograman berorientasi objek.</p>
       <h2>Jenis-Jenis Framework PHP</h2>
       <h3>1. Laravel</h3>
-      <p>Laravel adalah framework terkenal yang digunakan mengembangkan aplikasi yang sangat kuat. Framework ini sangat fleksibel dan dapat diskalakan dengan menawarkan fitur khusus rentang tinggi yang mungkin Anda butuhkan. Baca selengkapnya: Pengertian Framework Laravel Beserta Fitur dan Keunggulannya
-
-        Dengan Laravel Anda dapat menerapkan teknologi ORM, mendukung arsitektur MVC, dan menggunakan query builder secara langsung untuk mengakses basis data. Laravel menawarkan framework yang sangat aman.
-        
-        Ada beberapa keunggulan Laravel seperti dokumentasi mudah, ketahanan, keamanan, kecepatan tinggi, waktu singkat, dan fleksibilitas. Namun, Laravel juga memiliki kelemahan seperti memiliki beberapa bug.</p>
+      <p>Laravel adalah framework terkenal yang digunakan mengembangkan aplikasi yang sangat kuat. Framework ini sangat fleksibel dan dapat diskalakan dengan menawarkan fitur khusus rentang tinggi yang mungkin Anda butuhkan. Baca selengkapnya: Pengertian Framework Laravel Beserta Fitur dan Keunggulannya Dengan Laravel Anda dapat menerapkan teknologi ORM, mendukung arsitektur MVC, dan menggunakan query builder secara langsung untuk mengakses basis data. Laravel menawarkan framework yang sangat aman.      Ada beberapa keunggulan Laravel seperti dokumentasi mudah, ketahanan, keamanan, kecepatan tinggi, waktu singkat, dan fleksibilitas. Namun, Laravel juga memiliki kelemahan seperti memiliki beberapa bug.</p>
       <h3>2. CakePHP</h3>
-      <p>CakePHP dirilis pada tahun 2000 dan berada di daftar teratas framework PHP selama bertahun-tahun dengan pembaruan yang terus-menerus. Salah satu framework terbaik dan mudah digunakan untuk pemula dan ahli adalah CakePHP yang konsisten dengan rapid application development.
-
-        CakePHP adalah salah satu pilihan terbaik karena kesederhanaan dan skalabilitasnya untuk memberikan dukungan yang bagus bagi sistem.</p>
+      <p>CakePHP dirilis pada tahun 2000 dan berada di daftar teratas framework PHP selama bertahun-tahun dengan pembaruan yang terus-menerus. Salah satu framework terbaik dan mudah digunakan untuk pemula dan ahli adalah CakePHP yang konsisten dengan rapid application development. CakePHP adalah salah satu pilihan terbaik karena kesederhanaan dan skalabilitasnya untuk memberikan dukungan yang bagus bagi sistem.</p>
       <h3>3. Symfony</h3>
-      <p>Symfony membantu Anda mengembangkan aplikasi yang kuat dengan fitur-fiturnya yang paling bagus. Pada tahun 2005, Symfony diluncurkan dengan arsitektur MVC, penggunaan kembali kode, session management, routing URI yang fleksibel, dan dukungan platform yang berbeda untuk kelas basis data.
-
-        Beberapa kelebihan Symfony adalah tingkat keamanan yang tinggi terhadap banyak ancaman seperti pemalsuan dan permintaan lintas situs, akses server API, skalabilitas, kemampuan untuk memperluas, elemen dan konfigurasi independen, penggunaan memori yang kecil, stabil dan fleksibel.</p>
-
-        <h3>4. Zend</h3>
-        <p>Sulit untuk mengalahkan frame Zend jika Anda mencari aplikasi industri skala besar. Framework ini menyediakan hampir semua fitur OOP menjadi framework berorientasi objek seperti enkapsulasi dll. Zend terintegrasi dengan baik pada lingkungan yang memfasilitasi pengembangan produk software  berkualitas tinggi untuk perusahaan.
-    
-          Framework ini sesuai dengan praktik terbaik PHP dengan penyesuaian dan fleksibilitas tinggi yang membantu menambah fungsionalitas sesuai kebutuhan Anda.</p>
-        
-        <h3>5. CodeIgniter</h3>
-
-        <p>Framework CodeIgniter dirilis pada tahun 2006 untuk membangun aplikasi web yang dapat diskalakan. CodeIgniter telah diakui sebagai framework yang paling sederhana, kecepatan tinggi, dan mudah digunakan untuk kinerja tinggi. CodeIgniter terdiri dari sejumlah besar fitur dengan panduan, tutorial, dan banyak lagi untuk untuk mengembangkan dan memahami fitur-fiturnya dengan lebih baik. CodeIgniter memiliki keamanan tinggi, ringan, mendukung berbagai basis data dan platform, dan fitur enkripsi data tinggi. CodeIgniter menonjol karena kecepatannya dibandingkan dengan framework lain seperti dengan tugas-tugas database yang bekerja lebih cepat. Programmer pemula dapat dengan mudah memahami framework saat membandingkan dengan ZEND Framework. Baca selengkapnya tentang codeigniter
-        </p>
-    
-    
+      <p>Symfony membantu Anda mengembangkan aplikasi yang kuat dengan fitur-fiturnya yang paling bagus. Pada tahun 2005, Symfony diluncurkan dengan arsitektur MVC, penggunaan kembali kode, session management, routing URI yang fleksibel, dan dukungan platform yang berbeda untuk kelas basis data. Beberapa kelebihan Symfony adalah tingkat keamanan yang tinggi terhadap banyak ancaman seperti pemalsuan dan permintaan lintas situs, akses server API, skalabilitas, kemampuan untuk memperluas, elemen dan konfigurasi independen, penggunaan memori yang kecil, stabil dan fleksibel.</p>
+      <h3>4. Zend</h3>
+      <p>Sulit untuk mengalahkan frame Zend jika Anda mencari aplikasi industri skala besar. Framework ini menyediakan hampir semua fitur OOP menjadi framework berorientasi objek seperti enkapsulasi dll. Zend terintegrasi dengan baik pada lingkungan yang memfasilitasi pengembangan produk software  berkualitas tinggi untuk perusahaan. Framework ini sesuai dengan praktik terbaik PHP dengan penyesuaian dan fleksibilitas tinggi yang membantu menambah fungsionalitas sesuai kebutuhan Anda.</p>
+      <h3>5. CodeIgniter</h3>
+      <p>Framework CodeIgniter dirilis pada tahun 2006 untuk membangun aplikasi web yang dapat diskalakan. CodeIgniter telah diakui sebagai framework yang paling sederhana, kecepatan tinggi, dan mudah digunakan untuk kinerja tinggi. CodeIgniter terdiri dari sejumlah besar fitur dengan panduan, tutorial, dan banyak lagi untuk untuk mengembangkan dan memahami fitur-fiturnya dengan lebih baik. CodeIgniter memiliki keamanan tinggi, ringan, mendukung berbagai basis data dan platform, dan fitur enkripsi data tinggi. CodeIgniter menonjol karena kecepatannya dibandingkan dengan framework lain seperti dengan tugas-tugas database yang bekerja lebih cepat. Programmer pemula dapat dengan mudah memahami framework saat membandingkan dengan ZEND Framework. Baca selengkapnya tentang codeigniter</p>    
     </div>
     <!-- Optional JavaScript; choose one of the two! -->
 
--- resources/views/index0252.blade.php
+++ resources/views/index0252.blade.php
@@ -36,6 +36,23 @@
         <h3>Oleh Ahmad Alfianto-1461900252</h3>
     </div>
     
+    <div class="card">
+      <h5 class="card-header">Featured</h5>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="container-fluid">
+        <a class="navbar-brand text-center" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+    </nav>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
